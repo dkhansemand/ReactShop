939d1867c2da8e68a2388fec87e2b84485a24677
Andreas Holm Hansson <3941133@RTS-365.dk>
2018-01-11 12:06:40 +0100

    Fixed parrent onLick trigger from add to cart event

--- src/Components/Products/productItem.component.js
+++ src/Components/Products/productItem.component.js
@@ -4,16 +4,22 @@ import { Button, Card, CardTitle, CardActions, CardText} from 'react-md'
 class ProductItem extends Component {
     constructor(props){
         super(props)
-        this.handleProductShow = this.handleProductShow.bind(this)
+        this.handleProductClick = this.handleProductClick.bind(this)
        this.state = {
            redirect: false,
            clickedId: 0
        }
     }
 
-    handleProductShow(prodID){
-        console.log('Product clicked: ', prodID)
-        this.setState({ redirect: true, clickedId: prodID})
+    handleProductClick(target, data){
+        if(target === 'cart'){
+            this.props.click(data)
+            
+            return;
+        }else if(target === 'show'){
+            this.setState({ redirect: true, clickedId: data})
+            return;
+        }
     }
     
     render(){
@@ -21,7 +27,7 @@ class ProductItem extends Component {
             return <Redirect to={'/Product/'+this.state.clickedId} />
         }
         return (
-            <Card onClick={ () => { this.handleProductShow(this.props.id) }} shadow={2} style={{background: '#eee', cursor: 'pointer'}}>
+            <Card onClick={ (e) => { this.handleProductClick('show',this.props.id) }} shadow={2} style={{background: '#eee', cursor: 'pointer'}}>
                 <CardTitle title="" style={{color: '#000'}}>
                     <img src={this.props.image} alt={this.props.title} style={{width: '100%', height: 'auto'}} />
                 </CardTitle>
@@ -32,7 +38,7 @@ class ProductItem extends Component {
                     <p>DKK {this.props.price} ,-</p>
                 </CardText>
                 <CardActions>
-                    <Button raised secondary iconChildren="add_shopping_cart" onClick={ () => { this.props.click(this.props) }}>Køb</Button>
+                    <Button raised secondary iconChildren="add_shopping_cart" onClick={ (e) => { e.stopPropagation(); this.handleProductClick('cart', this.props) }}>Køb</Button>
                 </CardActions>
             </Card>
         )
