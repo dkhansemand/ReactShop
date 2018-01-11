51cb1305b7ad3e6745e0634a61ad9b0f48e65a38
Andreas Holm Hansson <3941133@RTS-365.dk>
2018-01-11 09:06:08 +0100

    Test run

--- src/Components/nav.component.js
+++ src/Components/nav.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Router, Link } from 'react-router-dom'
 import { Badge, Header, HeaderRow, Icon, Navigation, Textfield } from 'react-mdl'
 class Navbar extends Component {
     constructor(props){
@@ -14,11 +14,13 @@ class Navbar extends Component {
           <Header waterfall hideTop>
           <HeaderRow title="ReactShop">
               <Navigation>
-                <Link to="/" className="mdl-navigation__link">Home</Link>
-                <Link to="/Products" className="mdl-navigation__link">Produkter</Link>
-                <Link to="/Basket" className="mdl-navigation__link">
-                  <Badge text={this.props.basket} overlap><Icon name="shopping_cart" /></Badge>
-                </Link>
+                
+                  <Link to="/" className="mdl-navigation__link">Home</Link>
+                  <Link to="/Products" className="mdl-navigation__link">Produkter</Link>
+                  <Link to="/Basket" className="mdl-navigation__link">
+                    <Badge text={this.props.basket} overlap><Icon name="shopping_cart" /></Badge>
+                  </Link>
+                
               </Navigation>
               
               <Textfield
